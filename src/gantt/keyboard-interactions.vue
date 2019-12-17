ad984ba8bb4e5d8cb323da1441df276a8800ac94
BanupriyaSelvaraj <56014875+BanupriyaSelvaraj@users.noreply.github.com>
2019-12-17 18:44:28 +0530

    Github Source Update for Vol 4

--- src/gantt/keyboard-interactions.vue
+++ src/gantt/keyboard-interactions.vue
@@ -19,7 +19,7 @@
         </ejs-gantt>
         </div>
     </div>
-        <div class="col-md-3 property-section" style="height: 450px;overflow: auto">
+    <div class="col-md-3 property-section" style="height: 450px;overflow: auto">
             <table id="property" title="Properties">
             <tr style="height: 50px">
                 <td style="width: 30%">
@@ -158,94 +158,94 @@
                 </td>
             </tr>
         </table>
-        </div>
+    </div>
     <div id="action-description">
-    <p>This demo showcases the keyboard shortcuts applicable on Gantt and also lists out in below descriptions, and this
-        demo also explains how those applicable shortcuts interacts with Gantt actions.
-    </p>
-<div id="description">
-    <p>
-        All the Gantt actions can be controlled via keyboard keys and are availed using the <code>allowKeyboard</code>
-        property, which is set to true by default. The applicable key combinations and their relative functionalities are listed below.
-    </p>
-    <p>
-        Gantt component features are segregated into individual feature-wise modules. To use a selection, inject the
-        <code>Selection</code> module using the <code>Gantt.Inject(Selection)</code> method.To use a toolbar, inject
-        the <code>Toolbar</code> module using the <code>Gantt.Inject(Toolbar)</code> method.To use a edit, inject the
-        <code>Edit</code> module using the <code>Gantt.Inject(Edit)</code> method.To use markers, inject the
-        <code>DayMarkers</code> module using the <code>Gantt.Inject(DayMarkers)</code> method.
-    </p>
-    <table>
-        <tr>
-            <td><b>Keys</b></td>
-            <td><b>Description</b></td>
-        </tr>
-        <tr>
-            <td><code>Home</code></td>
-            <td>First Row Selection</td>
-        </tr>
-        <tr>
-            <td><code>End</code></td>
-            <td>Last Row Selection</td>
-        </tr>
-        <tr>
-            <td><code>DownArrow</code></td>
-            <td>Move Row Selection Down</td>
-        </tr>
-        <tr>
-            <td><code>UpArrow</code></td>
-            <td>Move Row Selection Up</td>
-        </tr>
-        <tr>
-            <td><code>Ctrl + UpArrow</code></td>
-            <td>Collapse All</td>
-        </tr>
-        <tr>
-            <td><code>Ctrl + DownArrow</code></td>
-            <td>Expand All</td>
-        </tr>
-        <tr>
-            <td><code>Ctrl + Shift + UpArrow</code></td>
-            <td>Collapse Row</td>
-        </tr>
-        <tr>
-            <td><code>Ctrl + Shift + DownArrow</code></td>
-            <td>Expand Row</td>
-        </tr>
-        <tr>
-            <td><code>Enter</code></td>
-            <td>Save Request</td>
-        </tr>
-        <tr>
-            <td><code>Esc</code></td>
-            <td>Cancel Request</td>
-        </tr>
-        <tr>
-            <td><code>Insert</code></td>
-            <td>Add Record</td>
-        </tr>
-        <tr>
-            <td><code>Ctrl + Insert</code></td>
-            <td>Add Record By Dialog</td>
-        </tr>
-        <tr>
-            <td><code>Ctrl + F2</code></td>
-            <td>Edit Record By Dialog</td>
-        </tr>
-        <tr>
-            <td><code>Delete</code></td>
-            <td>Delete Row</td>
-        </tr>
-        <tr>
-            <td><code>Ctrl + Shift + F</code></td>
-            <td>Focus Search Textbox</td>
-        </tr>
-        <tr>
-            <td><code>Shift + F5</code></td>
-            <td>Focus Task</td>
-        </tr>
-    </table>
-</div>
+         <p>This demo showcases the keyboard shortcuts applicable on Gantt and also lists out in below descriptions, and this
+            demo also explains how those applicable shortcuts interacts with Gantt actions.</p>
+    </div>
+    <div id="description">
+        <p>
+            All the Gantt actions can be controlled via keyboard keys and are availed using the <code>allowKeyboard</code>
+            property, which is set to true by default. The applicable key combinations and their relative functionalities are listed below.
+        </p>
+        <p>
+           Gantt component features are segregated into individual feature-wise modules. To use a selection, inject the
+           <code>Selection</code> module using the <code>Gantt.Inject(Selection)</code> method.To use a toolbar, inject
+           the <code>Toolbar</code> module using the <code>Gantt.Inject(Toolbar)</code> method.To use a edit, inject the
+           <code>Edit</code> module using the <code>Gantt.Inject(Edit)</code> method.To use markers, inject the
+           <code>DayMarkers</code> module using the <code>Gantt.Inject(DayMarkers)</code> method.
+        </p>
+        <table>
+            <tr>
+                <td><b>Keys</b></td>
+                <td><b>Description</b></td>
+            </tr>
+            <tr>
+                <td><code>Home</code></td>
+                <td>First Row Selection</td>
+            </tr>
+            <tr>
+                <td><code>End</code></td>
+                <td>Last Row Selection</td>
+            </tr>
+            <tr>
+                <td><code>DownArrow</code></td>
+                <td>Move Row Selection Down</td>
+            </tr>
+            <tr>
+                <td><code>UpArrow</code></td>
+                <td>Move Row Selection Up</td>
+            </tr>
+            <tr>
+                <td><code>Ctrl + UpArrow</code></td>
+                <td>Collapse All</td>
+            </tr>
+            <tr>
+                <td><code>Ctrl + DownArrow</code></td>
+                <td>Expand All</td>
+            </tr>
+            <tr>
+                <td><code>Ctrl + Shift + UpArrow</code></td>
+                <td>Collapse Row</td>
+            </tr>
+            <tr>
+                <td><code>Ctrl + Shift + DownArrow</code></td>
+                <td>Expand Row</td>
+            </tr>
+            <tr>
+                <td><code>Enter</code></td>
+                <td>Save Request</td>
+            </tr>
+            <tr>
+                <td><code>Esc</code></td>
+                <td>Cancel Request</td>
+            </tr>
+            <tr>
+                <td><code>Insert</code></td>
+                <td>Add Record</td>
+            </tr>
+            <tr>
+                <td><code>Ctrl + Insert</code></td>
+                <td>Add Record By Dialog</td>
+            </tr>
+            <tr>
+                <td><code>Ctrl + F2</code></td>
+                <td>Edit Record By Dialog</td>
+            </tr>
+            <tr>
+                <td><code>Delete</code></td>
+                <td>Delete Row</td>
+            </tr>
+            <tr>
+                <td><code>Ctrl + Shift + F</code></td>
+                <td>Focus Search Textbox</td>
+            </tr>
+            <tr>
+                <td><code>Shift + F5</code></td>
+                <td>Focus Task</td>
+            </tr>
+        </table>
+    </div>
 </div>
 </template>
 <script>
